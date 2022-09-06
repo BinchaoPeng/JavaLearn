963cc2fb39057055113088cec9f46db8b4278c03
pbc-win <646047990@qq.com>
2022-09-06 22:00:42 +0800

    assert

--- src/MyAssert/MyAssertDemo1.java
+++ src/MyAssert/MyAssertDemo1.java
@@ -4,13 +4,17 @@ package MyAssert;
  * @author pbc
  */
 public class MyAssertDemo1 {
-    public static void func(boolean assertFlag){
+    public static void func(Boolean assertFlag) {
+        assert assertFlag != null : "assertFlag is null";
         System.out.println(assertFlag);
         assert assertFlag : "coming assert";
         System.out.println("after assert");
     }
 
     public static void main(String[] args) {
+        //precondition, check param
+        func(null);
+
         func(true);
 
         //when false, coming assert
