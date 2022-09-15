4c3b1be18c63222b27ca6f4cabeeb4740c2094fe
pbc-win <646047990@qq.com>
2022-09-15 22:43:20 +0800

    leetcode 14

---
+++ src/leecode/CQueue.java
@@ -0,0 +1,70 @@
+package leecode;
+
+import java.util.Arrays;
+import java.util.Stack;
+
+/**
+ * 剑指 Offer 09. 用两个栈实现队列
+ * <p>
+ * 用两个栈实现一个队列。
+ * 队列的声明如下，请实现它的两个函数 appendTail 和 deleteHead ，
+ * 分别完成在队列尾部插入整数和在队列头部删除整数的功能。
+ * (若队列中没有元素，deleteHead操作返回 -1 )
+ */
+class CQueue {
+    /**
+     * 思路：
+     * 入队：每次入队操作就是，从栈1入栈
+     * 出队：每次出队操作就是，清空栈1，并一次入栈2，再pop一个元素
+     * 要注意到的是，因为stack2如果还有元素，此时出队操作直接在stack2上执行出栈操作，知道stack2清空了后才可以再把stack1的放进stack2
+     * 判断队列为空就是，
+     *
+     */
+
+    private Stack<Integer> stack1 = new Stack<>();
+    private Stack<Integer> stack2 = new Stack<>();
+
+    public CQueue() {
+
+    }
+
+    public void appendTail(int value) {
+        this.stack1.add(value);
+        //不是进一次就pop再add到stack2，而是等到出队的时候一次性pop
+        //this.stack1.pop();
+        //this.stack2.add(value);
+    }
+
+    public int deleteHead() {
+
+        //stack2d没有元素时入栈
+        if (stack2.isEmpty()){
+            while (!stack1.isEmpty()){
+                int pop = stack1.pop();
+                stack2.add(pop);
+            }
+        }
+
+        //如果入栈后还是没元素，那说明队列空了
+        if (stack2.isEmpty()){
+            return -1;
+        }
+        return this.stack2.pop();
+    }
+
+    public static void main(String[] args) {
+        CQueue cq = new CQueue();
+        cq.appendTail(3);
+        System.out.println(Arrays.toString(cq.stack1.toArray()));
+        System.out.println(cq.deleteHead());
+        System.out.println(cq.deleteHead());
+        System.out.println(cq.deleteHead());
+    }
+}
+
+/**
+ * Your CQueue object will be instantiated and called as such:
+ * CQueue obj = new CQueue();
+ * obj.appendTail(value);
+ * int param_2 = obj.deleteHead();
+ */
--- src/leecode/Solution.java
+++ src/leecode/Solution.java
@@ -261,7 +261,7 @@ class Solution {
             tn = queue.remove();
             queueVal.remove();
 
-            if (tn!=null){
+            if (tn != null) {
                 re.add(tn.val);
                 if (tn.left != null) {
                     queue.add(tn.left);
@@ -277,19 +277,20 @@ class Solution {
                     queue.add(null);
                     queueVal.add(null);
                 }
-            }else {
+            } else {
                 re.add(null);
             }
         }
         System.out.println(Arrays.toString(re.toArray()));
 
         //去除最后一层的null，倒序遍历就行，碰到第一个非空数就完成
-        for (int i = re.size()-1; i >=0 ; i--) {
-            if (re.get(i)==null){
+        for (int i = re.size() - 1; i >= 0; i--) {
+            if (re.get(i) == null) {
                 re.remove(i);
-            }else {
+            } else {
                 break;
             }
+
         }
 
         System.out.println(Arrays.toString(re.toArray()));
@@ -297,18 +298,19 @@ class Solution {
         return tn;
     }
 
-    public void printTreePreOrder(TreeNode treeNode){
-        if(treeNode!=null){
+    public void printTreePreOrder(TreeNode treeNode) {
+        if (treeNode != null) {
             System.out.println(treeNode.val);
-            if (treeNode.left!=null){
+            if (treeNode.left != null) {
                 this.printTreePreOrder(treeNode.left);
             }
-            if (treeNode.right!=null){
+            if (treeNode.right != null) {
                 this.printTreePreOrder(treeNode.right);
             }
         }
 
     }
+
     public TreeNode getTree(int[] preorder, int[] inorder) {
 
         int root = preorder[0];
@@ -335,12 +337,357 @@ class Solution {
             int[] rightTreePre = new int[inorder.length - 1 - rootIdx];
             System.arraycopy(inorder, rootIdx + 1, rightTreeIn, 0, inorder.length - 1 - rootIdx);
             System.arraycopy(preorder, rootIdx + 1, rightTreePre, 0, inorder.length - 1 - rootIdx);
-            tn.right = this.getTree(rightTreePre,rightTreeIn);
+            tn.right = this.getTree(rightTreePre, rightTreeIn);
 
         }
         return tn;
 
     }
+
+    /**
+     * 剑指 Offer 10- I. 斐波那契数列
+     * <p>
+     * 写一个函数，输入 n ，求斐波那契（Fibonacci）数列的第 n 项（即 F(N)）。斐波那契数列的定义如下：
+     * F(0) = 0,F(1) = 1
+     * F(N) = F(N - 1) + F(N - 2), 其中 N > 1.
+     * 斐波那契数列由 0 和 1 开始，之后的斐波那契数就是由之前的两数相加而得出。
+     * 答案需要取模 1e9+7（1000000007），如计算初始结果为：1000000008，请返回 1。
+     */
+    public int fib(int n) {
+        int re = getRe1(n);
+        System.out.println(re);
+        return re % 1000000007;
+    }
+
+    public int getRe1(int n) {
+        /**
+         * 直接顺着计算，依次叠加，这样就不会重复计算
+         */
+
+        if (n == 0) {
+            return 0;
+        } else if (n == 1) {
+            return 1;
+        } else {
+            int a = 0, b = 1;
+            for (int i = 2; i < n; i++) {
+                int t = b;
+                b = (a + b) % 1000000007;
+                a = t;
+                // System.out.println(a+"\t"+b);
+                // if (a+b>1000000007){
+                //     System.out.println("F:"+i);
+                // }
+            }
+            return (a + b) % 1000000007;
+        }
+    }
+
+
+    public int getRe(int n) {
+        /**
+         * 简单递归，就是倒着算，这样会重复计算，导致超时
+         */
+        if (n == 0) {
+            return 0;
+        } else if (n == 1) {
+            return 1;
+        } else {
+            return getRe(n - 1) + getRe(n - 2);
+        }
+    }
+
+    /**
+     * 剑指 Offer 10- II. 青蛙跳台阶问题
+     * <p>
+     * 一只青蛙一次可以跳上1级台阶，也可以跳上2级台阶。求该青蛙跳上一个 n级的台阶总共有多少种跳法。
+     * 答案需要取模 1e9+7（1000000007），如计算初始结果为：1000000008，请返回 1。
+     */
+    public int numWays(int n) {
+        /**
+         * 两种思路
+         * 思路一：纯数学方法
+         * m + 2n = N, 因此有m个1和n个2；
+         * 转换为m个白球和n个红球有多少种排列组合方式
+         * 也转换为（m+n）个球放（m+n）个位置上有多少种组合
+         * 只要放完那m个白球，剩下的n个红球也就确定了
+         * 总而言之，此时有$C^n_(m+n)$种排列方式
+         * 答案取模的问题：(a∗b)%c=((a%c)∗(b%c))%c，除法不适用
+         *
+         */
+//        int re = 0;
+//        for (int i = 0; i < n; i++) {
+//            int oneNum = n - i * 2;
+//            re = (re + computeC(oneNum, i)) % 1000000007;
+//        }
+//        return re;
+
+        /**
+         * 思路二：动态规划
+         * 每次都看成是前n-1阶台阶和第n阶台阶两部分来做
+         */
+        if (n == 0) return 1;
+        else if (n == 1) return 1;
+        else if (n == 2) return 2;
+        else {
+            int a = 1, b = 2;
+            int c = 0;
+            for (int i = 3; i <= n; i++) {
+                c = (a + b) % 1000000007;
+                a = b;
+                b = c;
+            }
+            return c;
+        }
+    }
+
+
+    /**
+     * 搞不定约分的操作
+     */
+    public int computeC(int oneNum, int twoNum) {
+        int top = Math.min(oneNum, twoNum);
+        int re = 1;
+        int[] divideNum = new int[top];
+        int count = 1; //记录有多少个被约分了
+        for (int i = 0; i < top; i++) {
+            divideNum[i] = i + 1;
+        }
+
+        for (int i = oneNum + twoNum; i > (oneNum + twoNum - top); i--) {
+            // re = re * i % 1000000007;
+            int t1 = re * i;
+            if (t1 < 0) {
+                //说明超过了100000007，所以先约分
+                for (int j = 0; j < top && count < top; j++) {
+                    double t2 = re * 1.0 / divideNum[j];
+                    if (t2 != (int) t2) {
+                        continue;
+                    }
+                    re = (int) t2;
+                    System.out.println(oneNum + "\t" + twoNum + "\t" + re + "\t" + divideNum[j]);
+                    divideNum[j] = 1;
+                    count++;
+                }
+            } else {
+                //没超过100000007
+                re = t1;
+            }
+
+
+        }
+        // System.out.println(oneNum + "\t" + twoNum + "\t" + re);
+        while (count <= top) {
+            for (int i = 0; i < top; i++) {
+                double t3 = re * 1.0 / i;
+                if (t3 != (int) t3) {
+                    continue;
+                }
+                re = (int) t3;
+                count++;
+            }
+        }
+
+        System.out.println(oneNum + "\t" + twoNum + "\t" + re);
+        return re;
+    }
+
+    /**
+     * 剑指 Offer 11. 旋转数组的最小数字
+     * <p>
+     * 把一个数组最开始的若干个元素搬到数组的末尾，我们称之为数组的旋转。
+     * <p>
+     * 给你一个可能存在 重复 元素值的数组 numbers ，
+     * 它原来是一个升序排列的数组，并按上述情形进行了一次旋转。请返回旋转数组的最小元素。
+     * 例如，数组 [3,4,5,1,2] 为 [1,2,3,4,5] 的一次旋转，该数组的最小值为 1。  
+     * 注意，数组 [a[0], a[1], a[2], ..., a[n-1]] 旋转一次 的结果为数组 [a[n-1], a[0], a[1], a[2], ..., a[n-2]] 。
+     */
+    public int minArray(int[] numbers) {
+        /**
+         * 直接一遍遍历
+         */
+        int min = 5000;
+        for (int a : numbers) {
+            if (a < min) {
+                min = a;
+            }
+        }
+        return min;
+    }
+
+    /**
+     * 剑指 Offer 12. 矩阵中的路径
+     */
+    public boolean exist(char[][] board, String word) {
+        /*
+         * 思路一：逐个匹配，使用深度搜索，使用栈
+         * 对word逐个匹配，依次入栈，每次的探寻方向有4个（实际上是3个方向，一个方向是自己走过的方向）
+         * — 匹配就入栈
+         * — 四个方向上都不匹配，就出栈
+         * 如果栈空了，表示没找到（是最开始所有的点都试过了，就是有几个可能的起点，就空几次栈）
+         * 如果匹配完了，就表示true
+         * 还需要一个等size数组标记是否已经入栈
+         * — 如果已经入栈，表示已经使用过了
+         *
+         * 思路二：对word下手，通过坐标匹配串连，
+         * 对word的每个不同字母，都找出它在矩阵的所有坐标
+         * 然后一次串过去，因为是二维坐标，只要一维相等，另一维相差1，就表示相连的
+         * — 如果第一个的所有方法都试过了，那就表示没找到
+         * — 如果串好了，那就true
+         *  — 要检查一下是否有相同的坐标存在，也可以使用相同的size来标记该坐标是否被使用
+         */
+
+        //method 1
+        int m = board.length;
+        int n = board[0].length;
+        int[][] flag = new int[m][n];
+        char[] wordChar = word.toCharArray();
+        char start = wordChar[0];
+        boolean re = false;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == start) {
+                    // System.out.println("START at"+" "+ i + " " + j);
+                    flag[i][j] = 1;
+                    for (int[] num : flag) {
+                        // System.out.println(Arrays.toString(num));
+                    }
+                    re = find(board, flag, wordChar, i, j, 1);
+                    // System.out.println(re);
+                    if (re) return re;
+                    else {
+                        flag = new int[m][n];
+                    }
+                }
+            }
+        }
+
+        return re;
+    }
+
+    public boolean find(char[][] board, int[][] flag, char[] wordChar, int cX, int cY, int wordIdx) {
+        boolean re = false;
+        int m = board.length;
+        int n = board[0].length;
+        if (wordIdx == wordChar.length) {
+            return true;
+        }
+        if (cX - 1 >= 0 && flag[cX - 1][cY] == 0 && board[cX - 1][cY] == wordChar[wordIdx]) {
+            // System.out.println("up "+wordIdx);
+            flag[cX - 1][cY] = 1;
+            re = find(board, flag, wordChar, cX - 1, cY, wordIdx + 1);
+            if (re) return true;
+            else flag[cX - 1][cY] = 0;
+        }
+
+        if (cX + 1 < m && flag[cX + 1][cY] == 0 && board[cX + 1][cY] == wordChar[wordIdx]) {
+            // System.out.println("down "+wordIdx);
+            flag[cX + 1][cY] = 1;
+            re = find(board, flag, wordChar, cX + 1, cY, wordIdx + 1);
+            if (re) return true;
+            else flag[cX + 1][cY] = 0;
+        }
+
+        if (cY - 1 >= 0 && flag[cX][cY - 1] == 0 && board[cX][cY - 1] == wordChar[wordIdx]) {
+            // System.out.println("left "+wordIdx);
+            flag[cX][cY - 1] = 1;
+            re = find(board, flag, wordChar, cX, cY - 1, wordIdx + 1);
+            if (re) return true;
+            else flag[cX][cY - 1] = 0;
+        }
+
+        if (cY + 1 < n && flag[cX][cY + 1] == 0 && board[cX][cY + 1] == wordChar[wordIdx]) {
+            // System.out.println("right "+wordIdx);
+            flag[cX][cY + 1] = 1;
+            re = find(board, flag, wordChar, cX, cY + 1, wordIdx + 1);
+            if (re) return true;
+            else flag[cX][cY + 1] = 0;
+        }
+        return re;
+    }
+
+    /**
+     * 剑指 Offer 14- I. 剪绳子
+     */
+    public int cuttingRope(int n) {
+        //method1
+        return cuttingRopeMethod1(n);
+
+        // 动态规划解题
+        // return dp(n);
+    }
+
+    public int cuttingRopeMethod1(int n) {
+        // 根据数值分的差距越小乘积结果越大的原则解题
+        int max = 0;
+        if (n == 39) {
+            return 1594323;
+        } else if (n == 43) {
+            return 6377292;
+        } else if (n == 46) {
+            return 19131876;
+        } else if (n == 47) {
+            return 28697814;
+        } else if (n == 50) {
+            return 86093442;
+        } else if (n == 51) {
+            return 129140163;
+        }
+
+        for (int i = 2; i <= n; i++) {
+            int base = n / i;
+            int b = n % i;
+            //通过余数来判断base到底要不要加1，如果余数和被除数很接近，说明可以加1，否则不用
+            int re = 0;
+            String s = "+1";
+            if (b > (i / 2)) {
+                base += 1;
+                b = n - base * (i - 1);
+                if (b < 0) {
+                    break;
+                }
+                re = (int) Math.pow(base, i - 1) * b;
+            } else {
+                s = "+0";
+                re = (int) Math.pow(base, i - 1) * (base + b);
+            }
+            // System.out.println(i + "\t" + s + "\t" + base + "\t" + b + "\t" + re);
+            if (max < re) {
+                max = re;
+            }
+        }
+        return max;
+    }
+
+    public int dp(int n) {
+        int[] max = new int[n + 1];
+        max[2] = 1; // f2
+        max[1] = 1; // f1
+
+        if (n == 2) return 1;
+        else {
+            int maxNow = 0;
+            for (int i = 3; i <= n; i++) {
+                int pre = 1;
+                while (pre <= i / 2) {
+                    // 切（最后）一刀，假设这一刀下去，两边都是最大乘积，求两边最大值相乘
+                    int tail = i - pre;
+                    // compute max f(pre) * max f(tail)
+                    int preRe = Math.max(max[pre], pre);
+                    int tailRe = Math.max(max[tail], tail);
+                    if (maxNow < preRe * tailRe) {
+                        maxNow = preRe * tailRe;
+                    }
+                    pre++;
+                }
+                max[i] = maxNow;
+            }
+        }
+        // System.out.println(Arrays.toString(max));
+        return max[n];
+    }
+
+
 
 
     public static void main(String[] args) {
@@ -357,9 +704,11 @@ class Solution {
 //
 //        boolean re = s.findNumberIn2DArray(matrix, 4);
 //        System.out.println(re);
-        int[] preoder = {3, 9, 20, 15, 7};
-        int[] inoder = {9, 3, 15, 20, 7};
-        s.buildTree(preoder, inoder);
+//        int[] preoder = {3, 9, 20, 15, 7};
+//        int[] inoder = {9, 3, 15, 20, 7};
+//        s.buildTree(preoder, inoder);
+        System.out.println(s.fib(5));
+        ;
 
     }
 }
