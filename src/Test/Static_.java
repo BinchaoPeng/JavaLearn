6189ac190f69fc892a96a47b4656f0b8a1789faa
pbc-win <646047990@qq.com>
2022-10-20 20:28:58 +0800

    static静态变量和代码块

--- src/Test/Static_.java
+++ src/Test/Static_.java
@@ -1,7 +1,11 @@
 package Test;
 
 public class Static_ {
+    int k = 1, j = 1;
+    static int i = 10;
+
     static {
+        i += 5;
         System.out.println("1");
     }
 
@@ -9,16 +13,18 @@ public class Static_ {
         System.out.println("2");
     }
 
-    int i = 1, j = 1;
 
     public static void main(String[] args) {
         System.out.println("m");
         Static_ static_ = new Static_();
         System.out.println("main");
+        System.out.println(static_.i);
+        System.out.println(Static_.i);
     }
 
 
     static {
+        i /= 3;
         System.out.println("3");
     }
 }
